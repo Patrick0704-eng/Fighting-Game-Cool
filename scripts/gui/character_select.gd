aeeae05f7d0ccc63cff5c10f5702aa3f4dc502ab
yomccc <yomccc574@gmail.com>
2024-10-29 09:53:48 +1100

    roblox

--- scripts/gui/character_select.gd
+++ scripts/gui/character_select.gd
@@ -3,8 +3,14 @@ extends Control
 @onready var cursor_2 = $player_2_cursor
 @onready var grid_1 = $player_1
 @onready var grid_2 = $player_2
+@onready var hon_1 = $player_1/hon_1
+@onready var zhin_1 = $player_1/zhin_1
+@onready var hon_2 = $player_2/hon_2
+@onready var zhin_2 = $player_2/zhin_2
 var locked_1
 var locked_2
+var player_1_array = [hon_1, zhin_1]
+var player_2_array = [hon_2, zhin_2]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -12,6 +18,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	cursor_1.position = player_1_array[global.player_1_character - 1].position
 	if Input.is_action_just_pressed("lock_1"):
 		locked_1 = true
 	if Input.is_action_just_pressed("lock_2"):
