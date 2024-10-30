9c52ce8c866a20eb9ff340886f173bc4edae76b9
yomccc <yomccc574@gmail.com>
2024-10-31 10:27:43 +1100

    bumcheeks

--- scripts/gui/character_select.gd
+++ scripts/gui/character_select.gd
@@ -7,6 +7,7 @@ extends Control
 @onready var zhin = $player_1/zhin
 @onready var hon_2 = $player_2/hon_2
 @onready var zhin_2 = $player_2/zhin_2
+@onready var dual_select = $dual_select
 var locked_1
 var locked_2
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +17,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if global.player_1_character == 1:
+		cursor_1.position = Vector2(100,32)	
+	if global.player_2_character == 1:
+		cursor_2.position = Vector2(100,32)
+	if global.player_1_character == 2:
+		cursor_1.position = Vector2(164,32)
+	if global.player_2_character == 2:
+		cursor_2.position = Vector2(164,32)
+	if global.player_1_character == 1 and global.player_2_character == 1:
+		cursor_1.hide()
+		cursor_2.hide()
+		dual_select.position = Vector2(100,32)
+		dual_select.show()
+	elif global.player_1_character == 2 and global.player_2_character == 2:
+		cursor_1.hide()
+		cursor_2.hide()
+		dual_select.position = Vector2(164,32)
+		dual_select.show()
+	else:
+		dual_select.hide()
+		cursor_1.show()
+		cursor_2.show()
 	if Input.is_action_just_pressed("lock_1"):
 		locked_1 = true
 	if Input.is_action_just_pressed("lock_2"):
@@ -36,7 +59,7 @@ func _process(delta):
 				#cursor_2.position.x +=
 		if Input.is_action_just_pressed("left2"):
 			if global.player_2_character > 1:
-				global.player_1_character +=1
+				global.player_2_character -=1
 				#cursor_2.position.x -=
 	if locked_1 and locked_2:
 		get_tree().change_scene_to_file("res://scenes/maps/temple.tscn")
