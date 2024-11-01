c195761c0b463aedad0ba86e9bfc321a0c926fde
yomccc <yomccc574@gmail.com>
2024-11-01 14:47:38 +1100

    map select should be working

--- scripts/global/global.gd
+++ scripts/global/global.gd
@@ -7,8 +7,8 @@ var player_2_health = 100
 #Define the variables to hold each players' selected character (1 by default)
 var player_1_character = 1
 var player_2_character = 1
-
-
+#map number, 1 by default (temple)
+var map = 0
 
 func _ready():
 	pass
--- scripts/gui/map_select.gd
+++ scripts/gui/map_select.gd
@@ -4,14 +4,15 @@ extends Control
 @onready var dual_select = $dual_cursor
 #map selection number
 var map_select_1 = 1
-var map_select_2 = 2
+var map_select_2 = 1
 #sees if selected map has been locked
 var locked_1 = false
 var locked_2 = false
 #randomises whether player 1 or 2 will have priority
-var rand_map = randi_range(1,2)
-#the map decided
-var map
+var pick_player_map = randi_range(1,2)
+#random map button
+var random_map = randi_range(1,2)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -27,6 +28,10 @@ func _process(delta):
 		cursor_1.position = Vector2(164,32)
 	if map_select_2 == 2:
 		cursor_2.position = Vector2(164,32)
+	if map_select_1 == 3:
+		cursor_1.position = Vector2(228,32)
+	if map_select_2 == 3:
+		cursor_2.position = Vector2(228,32)
 	if map_select_1 == 1 and map_select_2 == 1:
 		cursor_1.hide()
 		cursor_2.hide()
@@ -36,6 +41,11 @@ func _process(delta):
 		cursor_1.hide()
 		cursor_2.hide()
 		dual_select.position = Vector2(164,32)
+		dual_select.show()
+	elif map_select_1 == 3 and map_select_2 == 3:
+		cursor_1.hide()
+		cursor_2.hide()
+		dual_select.position = Vector2(228,32)
 		dual_select.show()
 	else:
 		dual_select.hide()
@@ -47,33 +57,70 @@ func _process(delta):
 		locked_2 = true
 	if locked_1 and locked_2:
 		if map_select_1 == 1 and map_select_2 == 1:
-			map == 1
+			global.map = 1
 		if map_select_1 == 1 and map_select_2 == 2:
-			if rand_map == 1:
-				map == 1
-			if rand_map == 2:
-				map == 2
+			if pick_player_map == 1:
+				global.map = 1
+			if pick_player_map == 2:
+				global.map = 2
 		if map_select_1 == 2 and map_select_2 == 1:
-			if rand_map == 1:
-				map == 2
-			if rand_map == 2:
-				map == 1
+			if pick_player_map == 1:
+				global.map = 2
+			if pick_player_map == 2:
+				global.map = 1
 		if map_select_1 and map_select_2 == 2:
-			map == 2
-		if map == 1:
+			global.map = 2
+		if map_select_1 == 3 and map_select_2 == 1:
+			if pick_player_map == 1:
+				if random_map == 1:
+					global.map = 1
+				if random_map == 2:
+					global.map = 2
+			if pick_player_map == 2:
+				global.map = 1
+		if map_select_1 == 3 and map_select_2 == 2:
+			if pick_player_map == 1:
+				if random_map == 1:
+					global.map =1
+				if random_map == 2:
+					global.map = 2
+			if pick_player_map == 2:
+				global.map = 2
+		if map_select_1 == 3 and map_select_2 == 3:
+			if random_map == 1:
+				global.map = 1
+			if random_map == 2:
+				global.map = 2
+		if map_select_1 == 2 and map_select_2 == 3:
+			if pick_player_map == 1:
+				global.map = 2
+			if pick_player_map == 2:
+				if random_map == 1:
+					global.map = 1
+				if random_map == 2:
+					global.map = 2
+		if map_select_1 == 1 and map_select_2 == 3:
+			if pick_player_map == 1:
+				global.map = 1
+			if pick_player_map == 2:
+				if random_map == 1:
+					global.map = 1
+				if random_map == 2:
+					global.map= 2
+		if global.map == 1:
 			get_tree().change_scene_to_file("res://scenes/maps/temple.tscn")
-		if map == 2:
-			print("map 2")
+		if global.map == 2:
+			pass
 	if !locked_1:
 		if Input.is_action_just_pressed("right1"):
-			if map_select_1 < 2:
+			if map_select_1 < 3:
 				map_select_1 += 1
 		if Input.is_action_just_pressed("left1"):
 			if map_select_1 > 1:
 				map_select_1 -= 1
 	if !locked_2:
 		if Input.is_action_just_pressed("right2"):
-			if map_select_2 < 2:
+			if map_select_2 < 3:
 				map_select_2 += 1
 		if Input.is_action_just_pressed("left2"):
 			if map_select_2 > 1:
