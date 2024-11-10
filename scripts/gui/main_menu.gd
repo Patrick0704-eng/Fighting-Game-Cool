f49790068821a4ec1a66ddeaa4212056e76bc8e9
Patrick0704-eng <val0008@nhs.vic.edu.au>
2024-11-10 14:29:03 +1100

    Added main menu stuff

--- scripts/gui/main_menu.gd
+++ scripts/gui/main_menu.gd
@@ -11,7 +11,16 @@ func _process(_delta):
 		get_tree().change_scene_to_file("res://scenes/gui/character_select.tscn")
 	if Input.is_action_just_pressed("lock_2"):
 		get_tree().change_scene_to_file("res://scenes/gui/character_select.tscn")
+	
+	#Quit if the player presses escape
+	if Input.is_action_just_pressed("esc"):
+		#Add an "Are you sure you want to quit?"
+		get_tree().quit()
 
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/gui/character_select.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
